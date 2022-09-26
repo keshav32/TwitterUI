1d3e468877f08785b68c09ad51b7314383659139
vishukaushik <vishukaushik1998@gmail.com>
2022-09-26 17:10:02 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { TweetComponent } from './tweet/tweet.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './user/login/login.component';
+import { AuthGuard } from './auth/auth.guard';
+import { EditProfileComponent } from './user/edit-profile/edit-profile.component';
+import { MyTweetsComponent } from './tweet/my-tweets/my-tweets.component';
+import { PostTweetComponent } from './tweet/post-tweet/post-tweet.component';
+import { ResetPasswordComponent } from './user/reset-password/reset-password.component';
+import { EditTweetComponent } from './tweet/edit-tweet/edit-tweet.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'user/login', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent, canActivate:[AuthGuard]},
+  { path: 'tweet', component: TweetComponent ,
+    children: [
+      { path: 'my-tweets', component: MyTweetsComponent },
+      { path: 'post-tweet', component: PostTweetComponent },
+      { path: 'edit-tweet', component: EditTweetComponent }
+    ]
+  },
+  { path: 'user', component: UserComponent ,
+  children:[
+    { path: 'registration', component: RegistrationComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'edit-profile', component: EditProfileComponent},
+    { path: 'reset-password', component: ResetPasswordComponent },
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_URL: "https://tweetapibyvishu.azurewebsites.net/api/v1.0/tweets"
 };
 
 /*
